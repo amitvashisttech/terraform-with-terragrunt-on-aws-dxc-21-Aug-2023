9843d5584c8c4dfa6cf0e64e1a5186f8fc06e5e0
Amit Vashist <amitvashist7@outlook.com>
2023-08-24 06:42:52 +0000

    Exp2

---
+++ 16-GraphViz/Exp2/resource.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+
+resource "aws_instance" "web" {
+  count = 1
+
+  # Amazon Linux Image ID
+  ami = "ami-0ccabb5f82d4c9af5"
+
+  instance_type = "t2.micro"
+
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+
+resource "aws_instance" "db" {
+  count = 1
+
+  # Amazon Linux Image ID
+  ami = "ami-0ccabb5f82d4c9af5"
+
+  instance_type = "t2.micro"
+
+  lifecycle {
+    prevent_destroy = "false"
+  }
+
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+
